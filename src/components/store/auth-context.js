d91a265ba8896b65de826630d45d33fb4b8747e0
gdtknight <gdtknight@naver.com>
2023-02-03 22:14:25 +0900

    #3 - AuthContextProvider
    Component 의 역할을 분리하고, 기능을 집중시키는 것이 유지보수에 유리.
    
    이를 위해 Provider 를 구현하고, 각 Component에서 Context를 통해 필요한 기능에 접근한다.

--- src/components/store/auth-context.js
+++ src/components/store/auth-context.js
@@ -1,13 +1,51 @@
 // Component를 작성하는 것이 아니기 때문에
 // CamelCase 로 표시하지 않고, kebab-case 사용
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 // component를 만드는 것은 아니지만, component 객체를 저장하므로 Component 처럼 선언
 const AuthContext = React.createContext({
   isLoggedIn: false,
 
   // IDE 자동 완성 기능 등 편의를 위해 dummy function을 넣을수도 있다.
-  onLogout: () => { }
+  onLogout: () => { },
+  onLogin: (email, password) => { }
 });
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+
+    if (storedUserLoggedInInformation === '1') {
+      setIsLoggedIn(true);
+    }
+
+  }, []);
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
+    setIsLoggedIn(false);
+  }
+
+  const loginHandler = () => {
+    localStorage.setItem('isLoggedIn', '1');
+    setIsLoggedIn(true);
+  }
+
+  return (
+    <AuthContext.Provider
+      value={
+        {
+          isLoggedIn: isLoggedIn,
+          onLogout: logoutHandler,
+          onLogin: loginHandler,
+        }
+      }
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContext;
