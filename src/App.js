169e38f3961ee46a754ba121c84252dfdab60a7c
gdtknight <gdtknight@naver.com>
2023-02-05 21:57:29 +0900

    #12 - useCallback
    useEffect와 비슷하나 function object를 대상으로 적용한다 ?

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 
 import Button from './components/UI/Button/Button';
 import DemoOutput from './components/Demo/DemoOutput';
@@ -9,15 +9,25 @@ function App() {
   console.log("APP RUNNING");
 
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
-  const toggleParagraphHandler = () => {
-    setShowParagraph(prevShowParagraph => !prevShowParagraph);
+  // useCallback으로 함수를 감싸면
+  // App function이 재실행될 때마다 새로운 함수가 생성되는 것을 막을 수 있다.
+  const toggleParagraphHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowParagraph(prevShowParagraph => !prevShowParagraph);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(prevAllowToggle => !prevAllowToggle);
   };
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
       <DemoOutput show={false} />
+      <Button onClick={allowToggleHandler}>Allow Toggling!</Button>
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
