1042a104a60c6ad368967ae745a1b93864ea0ef2
gdtknight <gdtknight@naver.com>
2023-02-05 13:52:30 +0900

    #10 - apply Cart context

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -36,6 +36,7 @@ const AvailableMeals = () => {
     meal =>
       <MealItem
         key={meal.id}
+        id={meal.id}
         name={meal.name}
         description={meal.description}
         price={meal.price}
