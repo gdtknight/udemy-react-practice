1fd45e904b60de6037da055ece562a18917b081b
gdtknight <gdtknight@naver.com>
2023-02-09 19:02:08 +0900

    #20 - custom hook practice complete !!!!

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,22 +1,110 @@
+import useInput from '../hooks/use-input';
+
 const BasicForm = (props) => {
+  const isNotEmpty = value => value.trim() !== '';
+  const isEmail = value => value.includes('@');
+
+  // alias destructuring
+  const {
+    value: enteredFirstName,
+    isValid: firstNameIsValid,
+    hasError: firstNameHasError,
+    valueChangeHandler: firstNameChangeHandler,
+    inputBlurHandler: firstNameBlurHandler,
+    reset: firstNameReset,
+  } = useInput(isNotEmpty);
+
+  const {
+    value: enteredLastName,
+    isValid: lastNameIsValid,
+    hasError: lastNameHasError,
+    valueChangeHandler: lastNameChangeHandler,
+    inputBlurHandler: lastNameBlurHandler,
+    reset: lastNameReset,
+  } = useInput(isNotEmpty);
+
+  const {
+    value: enteredEmail,
+    isValid: emailIsValid,
+    hasError: emailHasError,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: emailReset,
+  } = useInput(isEmail);
+
+  let formIsValid = false;
+
+  if (firstNameIsValid && lastNameIsValid && emailIsValid) {
+    formIsValid = true;
+  }
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    if (!formIsValid) {
+      return;
+    }
+
+    console.log('Submitted!');
+    console.log(enteredFirstName + " " + enteredLastName + ", " + enteredEmail);
+
+
+    firstNameReset();
+    lastNameReset();
+    emailReset();
+  };
+
+  const firstNameClasses = firstNameHasError
+    ? 'form-control invalid'
+    : 'form-control';
+
+  const lastNameClasses = lastNameHasError
+    ? 'form-control invalid'
+    : 'form-control';
+
+  const emailClasses = emailHasError
+    ? 'form-control invalid'
+    : 'form-control';
+
   return (
-    <form>
+    <form onSubmit={formSubmitHandler}>
       <div className='control-group'>
-        <div className='form-control'>
+        <div className={firstNameClasses}>
           <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+          <input
+            type='text'
+            id='name'
+            value={enteredFirstName}
+            onChange={firstNameChangeHandler}
+            onBlur={firstNameBlurHandler}
+          />
+          {firstNameHasError && <p className='error-text'>First Name must not be empty</p>}
         </div>
-        <div className='form-control'>
+        <div className={lastNameClasses}>
           <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+          <input
+            type='text'
+            id='name'
+            value={enteredLastName}
+            onChange={lastNameChangeHandler}
+            onBlur={lastNameBlurHandler}
+          />
+          {lastNameHasError && <p className='error-text'>Last Name must not be empty</p>}
         </div>
       </div>
-      <div className='form-control'>
+      <div className={emailClasses}>
         <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+        <input
+          type='text'
+          id='name'
+          value={enteredEmail}
+          onChange={emailChangeHandler}
+          onBlur={emailBlurHandler}
+        />
+        {emailHasError && <p className='error-text'>E-mail must contains @</p>}
       </div>
       <div className='form-actions'>
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
